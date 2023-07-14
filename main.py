342c2aab13db14af89fd711cab17cd49ad15d188
Max Elinson <max.elinson@gmail.com>
2023-07-14 17:50:03 -0400

    new version; optimizing variables

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import random
-from classes import Light, Pedestrian, Environment
-from utility import forward
+from utility import forward, cross_first_sim, cross_second_sim
 from collections import Counter
 import matplotlib.pyplot as plt
-import numpy as np
 
 NS_CROSS_TIME = 10
 EW_CROSS_TIME = 10
-NS_BLOCK_TIME = 60
+NS_BLOCK_TIME = 50  # this value has no implications on the final result, it is constant between both scenarios
 
-NS_LIGHT_TIME = 35
-EW_LIGHT_TIME = 25
-LIGHT_TIME = NS_LIGHT_TIME + EW_LIGHT_TIME
+NS_LIGHT_TIME = 0
+EW_LIGHT_TIME = 0
 
 MIN_GO_TIME = 5
 
@@ -19,101 +16,17 @@ MIN_GO_TIME = 5
 if __name__ == "__main__":
     #random.seed(42)
 
-    # Cross First
+    val_list = list(range(1, 100))
+
     cross_first_results = []
-    for i in range(1000):
-        light = Light(NS_LIGHT_TIME, EW_LIGHT_TIME, random.randint(0, LIGHT_TIME))
-        pedestrian = Pedestrian()
-        env = Environment(light, pedestrian)
-
-        # Cross EW
-        forward(EW_CROSS_TIME, 1, light, pedestrian)
-
-        # Walk up block
-        forward(NS_BLOCK_TIME, 1, light, pedestrian)
-
-        if light.ns_on and light.ns_time_left >= MIN_GO_TIME:
-            forward(NS_CROSS_TIME, 1, light, pedestrian)
-        else:
-            while not light.ns_on:
-                forward(1, 0, light, pedestrian)
-            forward(NS_CROSS_TIME, 1, light, pedestrian)
-        cross_first_results.append(pedestrian.elapsed_time)
-
-        plt.plot(pedestrian.time_list, pedestrian.distance_list, 'r-', alpha=0.1)
-    #plt.show()
-
-    # Cross Second
     cross_second_results = []
-    for i in range(1000):
-        light = Light(NS_LIGHT_TIME, EW_LIGHT_TIME, random.randint(0, LIGHT_TIME))
-        pedestrian = Pedestrian()
+    for i in val_list:
+        NS_LIGHT_TIME = i
+        EW_LIGHT_TIME = 100 - i
+        cross_first_results.append(cross_first_sim(NS_LIGHT_TIME, EW_LIGHT_TIME, NS_CROSS_TIME, EW_CROSS_TIME, MIN_GO_TIME, NS_BLOCK_TIME))
+        cross_second_results.append(cross_second_sim(NS_LIGHT_TIME, EW_LIGHT_TIME, NS_CROSS_TIME, EW_CROSS_TIME, MIN_GO_TIME, NS_BLOCK_TIME))
 
-        # Walk up block
-        forward(NS_BLOCK_TIME, 1, light, pedestrian)
-
-        if light.ns_on and light.ns_time_left >= MIN_GO_TIME:  # NS on and time left
-            forward(NS_CROSS_TIME, 1, light, pedestrian)
-            if light.ew_on and light.ew_time_left >= MIN_GO_TIME:  # EW on and time left
-                forward(EW_CROSS_TIME, 1, light, pedestrian)
-            else:  # Wait for EW
-                while not light.ew_on:
-                    forward(1, 0, light, pedestrian)
-                forward(EW_CROSS_TIME, 1, light, pedestrian)
-        else:
-            if light.ew_on and light.ew_time_left >= MIN_GO_TIME:  # EW on and time left
-                forward(EW_CROSS_TIME, 1, light, pedestrian)
-                if light.ns_on and light.ns_time_left >= MIN_GO_TIME:  # NS on and time left
-                    forward(NS_CROSS_TIME, 1, light, pedestrian)
-                else:  # Wait for NS
-                    while not light.ew_on:
-                        forward(1, 0, light, pedestrian)
-                    forward(NS_CROSS_TIME, 1, light, pedestrian)
-            else:  # Wait for EW
-                while not light.ew_on:
-                    forward(1, 0, light, pedestrian)
-                forward(EW_CROSS_TIME, 1, light, pedestrian)
-                if light.ns_on and light.ns_time_left >= MIN_GO_TIME:  # NS on and time left
-                    forward(NS_CROSS_TIME, 1, light, pedestrian)
-                else:  # Wait for NS
-                    while not light.ew_on:
-                        forward(1, 0, light, pedestrian)
-                    forward(NS_CROSS_TIME, 1, light, pedestrian)
-
-        cross_second_results.append(pedestrian.elapsed_time)
-
-        plt.plot(pedestrian.time_list, pedestrian.distance_list, 'b-', alpha=0.1)
-
-    first_results = Counter(cross_first_results)
-    print(first_results)
-    print(np.average(cross_first_results))
-    print(np.median(cross_first_results))
-    second_results = Counter(cross_second_results)
-    print(second_results)
-    print(np.average(cross_second_results))
-    print(np.median(cross_second_results))
+    plt.plot(val_list, cross_first_results, 'r-')
+    plt.plot(val_list, cross_second_results, 'b-')
 
     plt.show()
-
-
-
-
-    # first_results = Counter(cross_first_results)
-    # print(first_results)
-    #
-    # second_results = Counter(cross_second_results)
-    # print(second_results)
-    #
-    # lists1 = sorted(first_results.items())
-    # lists2 = sorted(second_results.items())
-    #
-    # x1, y1 = zip(*lists1)
-    # x2, y2 = zip(*lists2)
-    #
-    # fig, axs = plt.subplots(1, 2)
-    # axs[0].plot(x1, y1)
-    # axs[1].plot(x2, y2)
-    # plt.show()
-
-
-
--- utility.py
+++ utility.py
@@ -1,4 +1,7 @@
 from classes import Light, Pedestrian, Environment
+import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def forward(time: int, distance: int, light: Light, pedestrian: Pedestrian) -> None:
@@ -6,3 +9,74 @@ def forward(time: int, distance: int, light: Light, pedestrian: Pedestrian) -> N
         #env.step_time(distance)
         light.step_time()
         pedestrian.step_time(distance, 1)
+
+
+def cross_first_sim(ns_light_time, ew_light_time, ns_cross_time, ew_cross_time, min_go_time, ns_block_time) -> float:
+    # Cross First
+    cross_first_results = []
+    for i in range(500):
+        light = Light(ns_light_time, ew_light_time, random.randint(0, ns_light_time + ew_light_time))
+        pedestrian = Pedestrian()
+        env = Environment(light, pedestrian)
+
+        # Cross EW
+        forward(ew_cross_time, 1, light, pedestrian)
+
+        # Walk up block
+        forward(ns_block_time, 1, light, pedestrian)
+
+        if light.ns_on and light.ns_time_left >= min_go_time:
+            forward(ns_cross_time, 1, light, pedestrian)
+        else:
+            while not light.ns_on:
+                forward(1, 0, light, pedestrian)
+            forward(ns_cross_time, 1, light, pedestrian)
+        cross_first_results.append(pedestrian.elapsed_time)
+
+        # plt.plot(pedestrian.time_list, pedestrian.distance_list, 'r-', alpha=0.1)
+    return float(np.median(cross_first_results))
+
+
+def cross_second_sim(ns_light_time, ew_light_time, ns_cross_time, ew_cross_time, min_go_time, ns_block_time):
+    # Cross Second
+    cross_second_results = []
+    for i in range(500):
+        light = Light(ns_light_time, ew_light_time, random.randint(0, ns_light_time + ew_light_time))
+        pedestrian = Pedestrian()
+
+        # Walk up block
+        forward(ns_block_time, 1, light, pedestrian)
+
+        if light.ns_on and light.ns_time_left >= min_go_time:  # NS on and time left
+            forward(ns_cross_time, 1, light, pedestrian)
+            if light.ew_on and light.ew_time_left >= min_go_time:  # EW on and time left
+                forward(ew_cross_time, 1, light, pedestrian)
+            else:  # Wait for EW
+                while not light.ew_on:
+                    forward(1, 0, light, pedestrian)
+                forward(ew_cross_time, 1, light, pedestrian)
+        else:
+            if light.ew_on and light.ew_time_left >= min_go_time:  # EW on and time left
+                forward(ew_cross_time, 1, light, pedestrian)
+                if light.ns_on and light.ns_time_left >= min_go_time:  # NS on and time left
+                    forward(ns_cross_time, 1, light, pedestrian)
+                else:  # Wait for NS
+                    while not light.ew_on:
+                        forward(1, 0, light, pedestrian)
+                    forward(ns_cross_time, 1, light, pedestrian)
+            else:  # Wait for EW
+                while not light.ew_on:
+                    forward(1, 0, light, pedestrian)
+                forward(ew_cross_time, 1, light, pedestrian)
+                if light.ns_on and light.ns_time_left >= min_go_time:  # NS on and time left
+                    forward(ns_cross_time, 1, light, pedestrian)
+                else:  # Wait for NS
+                    while not light.ew_on:
+                        forward(1, 0, light, pedestrian)
+                    forward(ns_cross_time, 1, light, pedestrian)
+
+        cross_second_results.append(pedestrian.elapsed_time)
+
+        # plt.plot(pedestrian.time_list, pedestrian.distance_list, 'b-', alpha=0.1)
+    return float(np.median(cross_second_results))
+
